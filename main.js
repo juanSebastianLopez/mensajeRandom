41ec62c95215ddcb9029aca6d700ba0b531d0f0c
Juan Sebastian Lopez <104464559+juanSebastianLopez@users.noreply.github.com>
2022-12-14 17:31:40 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var btn = document.querySelector('#btn');
+var containerDescripcion = document.querySelector('.container__descripcion');
+var id = document.querySelector('#id');
+var img = document.querySelector('#img');
+var mensaje = document.querySelector('#descripcion');
+
+
+function dados(){
+fetch('https://api.adviceslip.com/advice')
+.then(res => res.json())
+.then(data => {
+        var valor = data.slip.id;
+        var descripcion = data.slip.advice;
+
+        id.append(valor);        
+        mensaje.append(descripcion);
+
+        btn.addEventListener('click',(e)=>{
+            img.classList.toggle('button-animation');
+        })
+    })
+}
+dados();
+
+btn.addEventListener('click', (e)=>{
+    const temporizador = setTimeout(()=>{
+        $('#id').empty();
+        $('#descripcion').empty();
+        dados();
+    },1500);
+})
